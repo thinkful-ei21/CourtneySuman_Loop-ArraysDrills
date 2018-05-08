04c8edade565767e34f592cbb375da3d5b73dc7a
Suman Bandita <sumanbandita@gmail.com>
2018-05-08 12:48:06 -0400

    function as arguments(2)

--- higherOrderFunc.js
+++ higherOrderFunc.js
@@ -15,3 +15,27 @@ function goodbye() {
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+//console.log(filteredNames) // => ['Rich', 'Ray']
+
+function filter(arr,fn) {
+	arr.forEach(element => {
+		if(fn(element))
+			console.log(element);		
+	});
+}
+	// let newArray = [];
+	// for(let i =0; i< arr.length ; i++){
+	// 	if(fn(arr[i]))
+	// 		newArray.push(arr[i]);
+	// }
+	// return newArray;
+
